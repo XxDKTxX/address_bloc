4bc5b9059841cd0d503af329d4f5a024c77d2f98
Devin Tassie <devintassie@gmail.com>
2018-02-06 00:43:34 +0000

    issue with test only being able to pass with csv file in specs folder

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,5 +1,6 @@
- # #8
+
  require_relative 'entry'
+ require "csv"
  
    class AddressBook
      attr_reader :entries
@@ -29,8 +30,17 @@
          break
        end
          index+= 1
-      end
-    
-      @entries.insert(index, Entry.new(name, phone, email))
+       end
+         @entries.insert(index, Entry.new(name, phone, email))
+     end
+     
+     def import_from_csv(file_name)
+       csv_text = File.read(file_name)
+       csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+ 
+       csv.each do |row|
+       row_hash = row.to_hash
+       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+     end
      end
    end
