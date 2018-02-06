3f9496fecbecc3460a9602bf5ba1f42695227a92
Devin Tassie <devintassie@gmail.com>
2018-02-06 00:52:59 +0000

    assignment complete

--- specs/address_book_spec.rb
+++ specs/address_book_spec.rb
@@ -47,4 +47,29 @@ RSpec.describe AddressBook do
        check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
      end
    end
+   
+      describe "attributes" do
+     it "responds to entries" do
+       expect(book).to respond_to(:entries)
+       book.import_from_csv("entries_2.csv")
+       # Check the first entry
+       entry_one = book.entries[0]
+ 
+       check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+     end
+
+     it "imports the 2nd entry" do
+       book.import_from_csv("entries.csv")
+       # Check the second entry
+       entry_two = book.entries[1]
+       check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+     end
+
+     it "imports the 3rd entry" do
+       book.import_from_csv("entries_2.csv")
+       # Check the third entry
+       entry_three = book.entries[2]
+       check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
+     end
    end
+  end
